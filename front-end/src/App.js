e1a6abf6b63b3c47868f63ac53fc19a6a38ddd71
Julian <juliandavidnunesfranco@hotmail.es>
2023-12-01 16:54:43 -0500

    feat:(landing page with botton ok in HTML)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,42 +1,17 @@
-import './App.css';
-import React from 'react';
-import { useSelector} from 'react-redux'  
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {LandingPage, Home, CreateVideogame, Detail}from './components'
+import React from "react";
 
 function App() {
-  const token = useSelector((state) => state.usersReducers.token);
-
-
-  if(token){
-    
-  return (
-    <BrowserRouter>
-          <React.Fragment>
-            <Routes>              
-            <Route exact path="/" component={LandingPage}/>
-            <Route path="/home" component={Home}/>
-            <Route path="/create" component={CreateVideogame}/>
-            <Route exact path="/videogames/:id" component={Detail}/>
-            </Routes>
-          </React.Fragment>           
-     </BrowserRouter>
-  )
-}else{
     return (
-      <BrowserRouter>
-          <React.Fragment>
-            <Routes>              
-            <Route exact path="/" component={LandingPage}/>
-            <Route path="/home" component={Home}/>
-            <Route path="/create" component={CreateVideogame}/>
-            <Route exact path="/videogames/:id" component={Detail}/>
-            </Routes>
-          </React.Fragment>           
-     </BrowserRouter>
-    )
-  
-}
+        <div
+            style={{
+                fontSize: "2em",
+                color: "blue",
+                fontFamily: "Arial, sans-serif",
+            }}
+        >
+            <h3>HOLA MUNDO</h3>
+        </div>
+    );
 }
 
 export default App;
