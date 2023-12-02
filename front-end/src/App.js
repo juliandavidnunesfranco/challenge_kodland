d044312b6fe55576ee7ee63da8cec6a84ea285dc
Julian <juliandavidnunesfranco@hotmail.es>
2023-12-02 16:01:22 -0500

    feat:(seccion about)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -59,13 +59,72 @@ function App() {
                                 <div className="mp-hero-content col-sm-6 col-xs-12">
                                     <h1>REACT.JS</h1>
                                     <h4>Libreria para front-end .</h4>
-                                    
                                 </div>
-                                
                             </div>
-                            
                         </div>
                         <Button />
+                    </div>
+                </div>
+
+                {/*Section text about the course*/}
+                <div className="mp-about-section section pt-150 pb-150">
+                    <div className="container">
+                        <div className="row">
+                            <div
+                                className="mp-section-title text-center col-xs-12 mb-60"
+                                data-name="REACT"
+                            >
+                                <h1>REACT.JS</h1>
+                            </div>
+                            <div className="mb-about-content text-center col-md-10 col-md-offset-1 col-xs-12">
+                                <p>
+                                    <p>
+                                        React.js, también conocido como React,
+                                        es una biblioteca de JavaScript de
+                                        código abierto para construir interfaces
+                                        de usuario. Desarrollado y mantenido por
+                                        Facebook, React se ha convertido en una
+                                        herramienta popular para el desarrollo
+                                        de aplicaciones web modernas y
+                                        eficientes.
+                                    </p>
+                                    <p>
+                                        Al utilizar un enfoque basado en
+                                        componentes, React facilita la creación
+                                        y el mantenimiento de interfaces de
+                                        usuario interactivas y reutilizables. Su
+                                        arquitectura modular permite dividir la
+                                        interfaz de usuario en componentes
+                                        independientes, lo que simplifica la
+                                        comprensión del código y fomenta la
+                                        reutilización de código en diferentes
+                                        partes de la aplicación.
+                                    </p>
+                                    <p>
+                                        Aunque React se originó en el contexto
+                                        de las aplicaciones web, su versatilidad
+                                        lo ha llevado más allá de las páginas
+                                        web. Ahora es comúnmente utilizado en el
+                                        desarrollo de aplicaciones móviles,
+                                        aplicaciones web progresivas (PWA) y
+                                        aplicaciones de escritorio. Su
+                                        flexibilidad y eficiencia en el
+                                        rendimiento lo convierten en una opción
+                                        poderosa para una variedad de proyectos
+                                        en el ámbito del desarrollo de software.
+                                    </p>
+                                </p>
+                                <a href="/artist">
+                                    Read More{" "}
+                                    <i className="fa fa-long-arrow-right" />
+                                    <img
+                                        src="/logo192.png"
+                                        alt="React Logo"
+                                        className="react-logo"
+                                    />
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </body>
