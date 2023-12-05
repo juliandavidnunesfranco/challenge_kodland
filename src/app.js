fba0f271802379c7ae000fcd7adfba2d47692739
Julian <juliandavidnunesfranco@hotmail.es>
2023-12-05 14:09:53 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(cors());
 
 //cors headers
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
         "GET, POST, OPTIONS, PUT, DELETE"
     );
     next();
-});
+}); */
 
 // limit requests from same API
 app.use(
